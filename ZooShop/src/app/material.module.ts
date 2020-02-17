9e92d9fbf26e3b0061d694d34700a36ca1bd9bf6
gregor3637 <mihailm956@gmail.com>
2020-02-17 22:06:22 +0200

    Sidenav navigation and responsive

--- ZooShop/src/app/material.module.ts
+++ ZooShop/src/app/material.module.ts
@@ -6,6 +6,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from "@angular/material/core";
 import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar"
+import { MatListModule } from "@angular/material/list"
 
 
 @NgModule({
@@ -17,6 +20,9 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
         MatDatepickerModule,
         MatNativeDateModule,
         MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
     ],
     exports: [
         MatButtonModule,
@@ -26,6 +32,9 @@ import { MatCheckboxModule } from "@angular/material/checkbox";
         MatDatepickerModule,
         MatNativeDateModule,
         MatCheckboxModule,
+        MatSidenavModule,
+        MatToolbarModule,
+        MatListModule,
     ]
 })
 
