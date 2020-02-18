b36e2b355064ca74b8a36c6028a22239b5700bdf
gregor3637 <mihailm956@gmail.com>
2020-02-18 23:59:25 +0200

    environments

--- ZooShop/src/environments/environment.prod.ts
+++ ZooShop/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBD4gbMW_ppSQ_4Hp7B6o19rbN-Hq2j6Co",
+    authDomain: "angular2020-9c230.firebaseapp.com",
+    databaseURL: "https://angular2020-9c230.firebaseio.com",
+    projectId: "angular2020-9c230",
+    storageBucket: "angular2020-9c230.appspot.com",
+    messagingSenderId: "804696263059",
+    appId: "1:804696263059:web:03b7850f3c0d8305dac066"
+  }
 };
--- ZooShop/src/environments/environment.ts
+++ ZooShop/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBD4gbMW_ppSQ_4Hp7B6o19rbN-Hq2j6Co",
+    authDomain: "angular2020-9c230.firebaseapp.com",
+    databaseURL: "https://angular2020-9c230.firebaseio.com",
+    projectId: "angular2020-9c230",
+    storageBucket: "angular2020-9c230.appspot.com",
+    messagingSenderId: "804696263059",
+    appId: "1:804696263059:web:03b7850f3c0d8305dac066"
+  }
 };
 
 /*
